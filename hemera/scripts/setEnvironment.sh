99a153dba509b3fe0e4b0f1b2cfa3c926cf5d035
Bertrand Benoit <hemerais@bertrand-benoit.net>
2010-09-03 12:40:34 +0000

    Updated INSTALL file information improving syntax of instructions. Created configuration system (1/2): instructions and samples.

--- hemera/scripts/setEnvironment.sh
+++ hemera/scripts/setEnvironment.sh
@@ -13,12 +13,17 @@
 [ -z "$installDir" ] && echo -e "This script must NOT be directly called. installDir variable not defined" >&2 && exit 1
 source "$installDir/scripts/utilities.sh"
 
+configurationFile="$installDir/config/hemera.conf"
+
 workDir="/tmp/Hemera"
 logDir="$workDir/Logs"
 fileDate=$(date +"%s")
 
 # Ensures various directories exists.
 mkdir -p "$workDir" "$logDir"
+
+# Ensures the system has been configured.
+[ ! -f "$configurationFile" ] && errorMessage "$configurationFile NOT found. You must configure the system (See $configurationFile.sample)."
 
 # Defines the log file if not already done.
 if [ -z "$logFile" ]; then
