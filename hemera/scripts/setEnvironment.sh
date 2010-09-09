7b8da4ccfe6649d48670b09d9e5a93ecdfca5005
Bertrand Benoit <hemerais@bertrand-benoit.net>
2010-09-09 07:15:54 +0000

    Added new configuration elements hemera.path.bin and hemera.path.lib allowing to specify path to add to PATH or LD_LIBRARY_PATH environment variables. Updated consequently INSTALL file, and environment setter.

--- hemera/scripts/setEnvironment.sh
+++ hemera/scripts/setEnvironment.sh
@@ -13,8 +13,16 @@
 [ -z "$installDir" ] && echo -e "This script must NOT be directly called. installDir variable not defined" >&2 && exit 1
 source "$installDir/scripts/utilities.sh"
 
+# Defines configuration file.
 configurationFile="$installDir/config/hemera.conf"
 
+# Updates environment path if needed.
+additionalBinPath=$( getConfigValue "hemera.path.bin" )
+additionalLibPath=$( getConfigValue "hemera.path.lib" )
+[ ! -z "$additionalBinPath" ] && export PATH=$additionalBinPath:$PATH
+[ ! -z "$additionalLibPath" ] && export LD_LIBRARY_PATH=$additionalLibPath:$LD_LIBRARY_PATH
+
+# Defines some global variables.
 workDir="/tmp/Hemera"
 logDir="$workDir/Logs"
 fileDate=$(date +"%s")
