f009c2bc06e940f5d58800ee8c5da91a7a6dd0a7
Bertrand Benoit <hemerais@bertrand-benoit.net>
2010-09-15 10:03:35 +0000

    sound player: updated to use Hemera Log system.

--- hemera/src/hemera/tools/LightSoundPlayer.java
+++ hemera/src/hemera/tools/LightSoundPlayer.java
@@ -20,7 +20,10 @@
 
 package hemera.tools;
 
+import hemera.log.Log;
 import hemera.utils.sound.SoundPlayer;
+
+import java.io.FileNotFoundException;
 
 /**
  * Hemera - Intelligent System Light pure java sound player.
@@ -30,58 +33,65 @@ import hemera.utils.sound.SoundPlayer;
  */
 public final class LightSoundPlayer {
 
-    /****************************************************************************************/
-    /*                                                                                      */
-    /* Constants */
-    /*                                                                                      */
-    /****************************************************************************************/
+	/****************************************************************************************/
+	/*                                                                                      */
+	/* Constants */
+	/*                                                                                      */
+	/****************************************************************************************/
 
-    /****************************************************************************************/
-    /*                                                                                      */
-    /* Attributes */
-    /*                                                                                      */
-    /****************************************************************************************/
+	/****************************************************************************************/
+	/*                                                                                      */
+	/* Attributes */
+	/*                                                                                      */
+	/****************************************************************************************/
 
-    /****************************************************************************************/
-    /*                                                                                      */
-    /* Constructors */
-    /*                                                                                      */
-    /****************************************************************************************/
+	/****************************************************************************************/
+	/*                                                                                      */
+	/* Constructors */
+	/*                                                                                      */
+	/****************************************************************************************/
 
-    /****************************************************************************************/
-    /*                                                                                      */
-    /* Getters / Setters */
-    /*                                                                                      */
-    /****************************************************************************************/
+	/****************************************************************************************/
+	/*                                                                                      */
+	/* Getters / Setters */
+	/*                                                                                      */
+	/****************************************************************************************/
 
-    /****************************************************************************************/
-    /*                                                                                      */
-    /* Specific methods */
-    /*                                                                                      */
-    /****************************************************************************************/
+	/****************************************************************************************/
+	/*                                                                                      */
+	/* Specific methods */
+	/*                                                                                      */
+	/****************************************************************************************/
 
-    /**
-     * @param args
-     */
-    public static final void main(final String[] args) {
-	if (args.length == 0) {
-	    System.err
-		    .println("usage: lightSoundPlayer <sound file path> [<sound file path> ... <sound file path>]");
-	    System.exit(1);
+	/**
+	 * @param args
+	 */
+	public static final void main(final String[] args) {
+		// Ensures at least one file sound path has been specified.
+		if (args.length == 0) {
+			System.err.println("usage: lightSoundPlayer <sound file path> [<sound file path> ... <sound file path>]");
+			System.exit(1);
+		}
+
+		try {
+			Log.utils.finest("Creating sound player");
+			// Creates a sound player.
+			final SoundPlayer soundPlayer = new SoundPlayer();
+			Log.utils.finer("Created sound player");
+
+			// For each specified sound file.
+			Log.utils.fine("Managing specified sound file to play");
+			for (final String soundFilePath : args) {
+				try {
+					soundPlayer.playFile(soundFilePath);
+				} catch (final FileNotFoundException e) {
+					Log.utils.warning("Unable to play '" + soundFilePath + "' (Message: " + e.getLocalizedMessage() + ").");
+				}
+			}
+		} catch (final Exception e) {
+			Log.utils.severe("An error occured while managing sound file to play (Message: " + e.getLocalizedMessage() + ").");
+			Log.manageThrowable(e, 1);
+		}
 	}
-
-	try {
-	    // Creates a sound player.
-	    final SoundPlayer soundPlayer = new SoundPlayer();
-
-	    // For each specified sound file.
-	    for (final String soundFilePath : args) {
-		soundPlayer.playFile(soundFilePath);
-	    }
-	} catch (final Exception e) {
-	    e.printStackTrace();
-	    System.exit(1);
-	}
-    }
 
 }
--- hemera/src/hemera/utils/sound/SoundPlayer.java
+++ hemera/src/hemera/utils/sound/SoundPlayer.java
@@ -20,6 +20,8 @@
 
 package hemera.utils.sound;
 
+import hemera.log.Log;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -40,114 +42,120 @@ import javax.sound.sampled.UnsupportedAudioFileException;
  */
 public final class SoundPlayer {
 
-    /****************************************************************************************/
-    /*                                                                                      */
-    /* Constants */
-    /*                                                                                      */
-    /****************************************************************************************/
+	/****************************************************************************************/
+	/*                                                                                      */
+	/* Constants */
+	/*                                                                                      */
+	/****************************************************************************************/
 
-    private static final int EXTERNAL_BUFFER_SIZE = 128000;
+	private static final int EXTERNAL_BUFFER_SIZE = 128000;
 
-    /****************************************************************************************/
-    /*                                                                                      */
-    /* Attributes */
-    /*                                                                                      */
-    /****************************************************************************************/
+	/****************************************************************************************/
+	/*                                                                                      */
+	/* Attributes */
+	/*                                                                                      */
+	/****************************************************************************************/
 
-    /****************************************************************************************/
-    /*                                                                                      */
-    /* Constructors */
-    /*                                                                                      */
-    /****************************************************************************************/
+	/****************************************************************************************/
+	/*                                                                                      */
+	/* Constructors */
+	/*                                                                                      */
+	/****************************************************************************************/
 
-    /****************************************************************************************/
-    /*                                                                                      */
-    /* Getters / Setters */
-    /*                                                                                      */
-    /****************************************************************************************/
+	/****************************************************************************************/
+	/*                                                                                      */
+	/* Getters / Setters */
+	/*                                                                                      */
+	/****************************************************************************************/
 
-    /****************************************************************************************/
-    /*                                                                                      */
-    /* Specific methods */
-    /*                                                                                      */
-    /****************************************************************************************/
+	/****************************************************************************************/
+	/*                                                                                      */
+	/* Specific methods */
+	/*                                                                                      */
+	/****************************************************************************************/
 
-    /**
-     * Plays the sound whose file path is specified.
-     * 
-     * @param filePath
-     *            the path of the sound to play.
-     * @throws FileNotFoundException
-     * @throws HemeraSoundException
-     */
-    public final void playFile(final String filePath)
-	    throws FileNotFoundException, HemeraSoundException {
-	final File soundFile = new File(filePath);
-
-	// Ensures it exists.
-	if (!soundFile.exists()) {
-	    throw new FileNotFoundException("Sound file '" + filePath + "'.");
-	}
-
-	/*
-	 * We have to read in the sound file.
+	/**
+	 * Plays the sound whose file path is specified.
+	 * 
+	 * @param filePath
+	 *            the path of the sound to play.
+	 * @throws FileNotFoundException
+	 * @throws HemeraSoundException
 	 */
-	AudioInputStream audioInputStream = null;
-	try {
-	    audioInputStream = AudioSystem.getAudioInputStream(soundFile);
-	} catch (final UnsupportedAudioFileException e) {
-	    throw new HemeraSoundException("Unable to get audioInputStream.", e);
-	} catch (final IOException e) {
-	    throw new HemeraSoundException("Unable to get audioInputStream.", e);
-	}
+	public final void playFile(final String filePath) throws FileNotFoundException, HemeraSoundException {
+		final File soundFile = new File(filePath);
 
-	// Prepares SourceDataLine.
-	final AudioFormat audioFormat = audioInputStream.getFormat();
-	SourceDataLine line = null;
-	final DataLine.Info info = new DataLine.Info(SourceDataLine.class,
-		audioFormat);
-	try {
-	    // Gets a line, and then opens it.
-	    line = (SourceDataLine) AudioSystem.getLine(info);
-	    line.open(audioFormat);
-	} catch (final LineUnavailableException e) {
-	    throw new HemeraSoundException(
-		    "Unable to open sound source data line.", e);
-	}
-
-	// Activates it.
-	line.start();
-
-	// Reads (buffer) data from sound file, and writes them to data line.
-	int audioDataBytesRead = 0;
-	final byte[] audioDataBuffer = new byte[EXTERNAL_BUFFER_SIZE];
-	while (audioDataBytesRead != -1) {
-	    try {
-		// Reads.
-		audioDataBytesRead = audioInputStream.read(audioDataBuffer, 0,
-			audioDataBuffer.length);
-
-		// Writes to data line (if needed).
-		if (audioDataBytesRead >= 0) {
-		    line.write(audioDataBuffer, 0, audioDataBytesRead);
+		// Ensures it exists.
+		if (!soundFile.exists()) {
+			throw new FileNotFoundException("Sound file '" + filePath + "'.");
 		}
-	    } catch (final IOException e) {
-		throw new HemeraSoundException(
-			"Error while reading sound file '" + filePath + "'.", e);
-	    }
+
+		Log.utils.info(filePath + ": managed new sound file.");
+
+		/*
+		 * We have to read in the sound file.
+		 */
+		Log.utils.finer(filePath + ": opening audio output stream.");
+		AudioInputStream audioInputStream = null;
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+			Log.utils.fine(filePath + ": opened audio output stream.");
+		} catch (final UnsupportedAudioFileException e) {
+			throw new HemeraSoundException("Unable to get audioInputStream.", e);
+		} catch (final IOException e) {
+			throw new HemeraSoundException("Unable to get audioInputStream.", e);
+		}
+
+		// Prepares SourceDataLine.
+		final AudioFormat audioFormat = audioInputStream.getFormat();
+		Log.utils.fine(filePath + ": format is " + audioFormat);
+		SourceDataLine line = null;
+		final DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+		try {
+			// Gets a line, and then opens it.
+			line = (SourceDataLine) AudioSystem.getLine(info);
+			line.open(audioFormat);
+		} catch (final LineUnavailableException e) {
+			throw new HemeraSoundException("Unable to open sound source data line.", e);
+		}
+
+		// Activates it.
+		Log.utils.finer(filePath + ": starting line.");
+		line.start();
+
+		// Reads (buffer) data from sound file, and writes them to data line.
+		Log.utils.finer(filePath + ": starting reading.");
+		int audioDataBytesRead = 0;
+		final byte[] audioDataBuffer = new byte[EXTERNAL_BUFFER_SIZE];
+		while (audioDataBytesRead != -1) {
+			try {
+				// Reads.
+				audioDataBytesRead = audioInputStream.read(audioDataBuffer, 0, audioDataBuffer.length);
+
+				// Writes to data line (if needed).
+				if (audioDataBytesRead >= 0) {
+					Log.utils.finest(filePath + ": read " + audioDataBytesRead + " bytes.");
+					line.write(audioDataBuffer, 0, audioDataBytesRead);
+				}
+			} catch (final IOException e) {
+				throw new HemeraSoundException("Error while reading sound file '" + filePath + "'.", e);
+			}
+		}
+
+		// Waits until all data are managed.
+		Log.utils.finer(filePath + ": draining line.");
+		line.drain();
+
+		// Finally closes the line.
+		Log.utils.finer(filePath + ": closing line.");
+		line.close();
+		Log.utils.fine(filePath + ": closed.");
 	}
 
-	// Waits until all data are managed.
-	line.drain();
-
-	// FInally closes the line.
-	line.close();
-    }
-
-    /****************************************************************************************/
-    /*                                                                                      */
-    /* Overrides */
-    /*                                                                                      */
-    /****************************************************************************************/
+	/****************************************************************************************/
+	/*                                                                                      */
+	/* Overrides */
+	/*                                                                                      */
+	/****************************************************************************************/
 
 }
