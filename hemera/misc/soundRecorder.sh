5c7d738ae1aeb175be81bb13c3d61359a3889efe
Bertrand Benoit <hemerais@bertrand-benoit.net>
2010-09-23 14:48:10 +0000

    sound recorder: improved messages, and some variables name.

--- hemera/misc/soundRecorder.sh
+++ hemera/misc/soundRecorder.sh
@@ -21,15 +21,15 @@ soundRecorderBin=$( getConfigPath "$CONFIG_KEY.soundRecorder.path" ) || exit 100
 soundRecorderOptions=$( getConfigValue "$CONFIG_KEY.soundRecorder.options" ) || exit 100
 
 # Defines the PID file.
-srPidFile="$pidDir/soundRecording.pid"
+pidFile="$pidDir/soundRecording.pid"
 
 #########################
 ## Functions
 # usage: usage
 function usage() {
   echo -e "Usage: $0 -S||-K [-hv]"
-  echo -e "-S\tstart endless sound recording"
-  echo -e "-K\tstop endless sound recording"
+  echo -e "-S\tstart sound recording daemon"
+  echo -e "-K\tstop sound recording daemon"
   echo -e "-v\tactivate the verbose mode"
   echo -e "-h\tshow this usage"
   echo -e "\nYou must either start or stop the sound recording."
@@ -66,29 +66,29 @@ checkBin "$soundRecorderBin" || exit 126
 ## INSTRUCTIONS
 case "$mode" in
   $MODE_DAEMON)
-    # Writes the PID file and launches the tool.
+    # Starts the process.
+    writeMessage "Launching Sound recorder daemon."
     input="$tmpEventDir/speech%3n.wav"
-    writeMessage "Launching Sound recorder."
-    startProcess "$srPidFile" "$soundRecorderBin" $( eval echo "$soundRecorderOptions" )    
+    startProcess "$pidFile" "$soundRecorderBin" $( eval echo "$soundRecorderOptions" )
   ;;
 
   $MODE_START)
     # Ensures it is not already running.
-    isRunningProcess "$srPidFile" "$soundRecorderBin" && writeMessage "Sound recorder is already running." && exit 0
+    isRunningProcess "$pidFile" "$soundRecorderBin" && writeMessage "Sound recorder daemon is already running." && exit 0
     
     # Starts it, launching this script in daemon mode.
     specificLogFile="$logFile.soundRecorder"
     logFile="$specificLogFile" "$0" -D >"$specificLogFile" 2>&1 &
-    writeMessage "Launched Sound recorder."
+    writeMessage "Launched Sound recorder daemon."
   ;;
 
   $MODE_STOP)
     # Ensures it is running.
-    ! isRunningProcess "$srPidFile" "$soundRecorderBin" && writeMessage "Sound recorder is NOT running." && exit 0
+    ! isRunningProcess "$pidFile" "$soundRecorderBin" && writeMessage "Sound recorder daemon is NOT running." && exit 0
   
     # Stops the process.
-    stopProcess "$srPidFile" "$soundRecorderBin" || errorMessage "Unable to stop sound recorder."  
-    writeMessage "Stopped Sound recorder."
+    stopProcess "$pidFile" "$soundRecorderBin" || errorMessage "Unable to stop sound recorder."  
+    writeMessage "Stopped Sound recorder daemon."
   ;;
   
   [?])  usage;;
