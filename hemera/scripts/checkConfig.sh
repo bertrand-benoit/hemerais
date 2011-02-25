b7fe2a9ab123078bba1331d3921abbe016be96f2
Bertrand Benoit <hemerais@bertrand-benoit.net>
2011-02-25 22:36:33 +0000

    Hemera strings internationalization:  - added English internationalization file  - updated checkConfig.sh to ensure internationalized files are ok (special characters are well escaped, i18n elements are well defined ...)  - added warning function

--- hemera/scripts/checkConfig.sh
+++ hemera/scripts/checkConfig.sh
@@ -42,3 +42,48 @@ for pathKey in $( grep -re "^[^#][a-zA-Z.]*[.]path=" "$h_configurationFile" |sed
   ! checkBin "$pathValue" && echo -e "$pathValue \E[31mNOT found\E[0m" && continue
   echo "OK"
 done
+
+# Checks locale files.
+writeMessage "Checking locale files (BEGIN)"
+refLocaleFile="$installDir/locale/hemera-i18n.fr"
+refLocaleFilePurified="$h_workDir/checkConfig_$( basename "$refLocaleFile" ).purified"
+extractI18Nelement "$refLocaleFile" "$refLocaleFilePurified"
+cat "$refLocaleFilePurified" |sed -e 's/=.*$//g;' > "$refLocaleFilePurified.keys"
+for localeFile in $( find "$installDir/locale" -maxdepth 1 -type f -regextype posix-extended -regex ".*\/hemera-i18n[.][^~]*" ); do
+  localFileName=$( basename "$localeFile" )
+  localFilePurified="$h_workDir/checkConfig_$localFileName.purified"
+
+  # Extracts i18n elements.
+  [[ "$localeFile" != "$refLocaleFile" ]] && extractI18Nelement "$localeFile" "$localFilePurified"
+
+  # Checks each definition.
+  for i18nElementRaw in $( grep -re "^[ \t]*[^#]" "$localeFile" |sed -e 's/[ \t]/€/g;' ); do
+    i18nElement=$( echo "$i18nElementRaw" |sed -e 's/€/ /g;' )
+
+    # Checks if there is a variable into this definition.
+    if [ $( echo "$i18nElement" |grep -E "[$]" |wc -l ) -gt 0 ]; then
+      # Ensures special characters are escaped.
+      if [ $( echo "$i18nElement" |grep -E "[^\\]['$]" |wc -l ) -gt 0 ]; then
+        warning "($localFileName) some characters should be escaped in: $i18nElement"
+      fi
+    else
+      # Ensures there is NO escaped characters.
+      if [ $( echo "$i18nElement" |grep -E "\\\\" |wc -l ) -gt 0 ]; then
+        warning "($localFileName) some characters should NOT be escaped in: $i18nElement"
+      fi
+    fi
+  done
+
+  # Checks if it is the reference locale file in which case there is nothing more to do.
+  [[ "$localeFile" == "$refLocaleFile" ]] && continue
+
+  # Ensures there is the same i18n elements of the reference locale file.
+  cat "$localFilePurified" |sed -e 's/=.*$//g;' > "$localFilePurified.keys"
+  diff "$refLocaleFilePurified.keys" "$localFilePurified.keys" > "$localFilePurified.keys.diff"
+  missingI18NElements=$( grep -re "^<" "$localFilePurified.keys.diff" |sed -e 's/</,/g;' |tr -d '\n' |sed -e 's/^,[ ]//' )
+  [ ! -z "$missingI18NElements" ] &&  warning "($localFileName) missing following i18n definition: $missingI18NElements"
+
+  unknownI18NElements=$( grep -re "^>" "$localFilePurified.keys.diff" |sed -e 's/>/,/g;' |tr -d '\n' |sed -e 's/^,[ ]//' )
+  [ ! -z "$unknownI18NElements" ] &&  warning "($localFileName) following i18n definition are unknown: $unknownI18NElements"
+done
+writeMessage "Checking locale files (END)"
