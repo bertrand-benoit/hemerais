a79bb6b2e8b7bd4271fcf36520b65bccea10daf0
Bertrand Benoit <hemerais@bertrand-benoit.net>
2011-04-25 15:22:43 +0000

    Main project: renamed used logger (tab has been replaced by space, don't care about all change).

--- Hemera/src/hemera/log/Log.java
+++ Hemera/src/hemera/log/Log.java
@@ -38,129 +38,131 @@ import java.util.logging.Logger;
  */
 public final class Log {
 
-	/****************************************************************************************/
-	/*                                                                                      */
-	/* Constants */
-	/*                                                                                      */
-	/****************************************************************************************/
+    /****************************************************************************************/
+    /*                                                                                      */
+    /* Constants */
+    /*                                                                                      */
+    /****************************************************************************************/
 
-	/** Property allowing definition of log file. */
-	private static final String LOG_FILE_PROPERTY = "hemera.log.file";
+    /** Property allowing definition of log file. */
+    private static final String LOG_FILE_PROPERTY = "hemera.log.file";
 
-	/** Log file. */
-	private static final String LOG_FILE = System.getProperty(LOG_FILE_PROPERTY);
+    /** Log file. */
+    private static final String LOG_FILE = System.getProperty(LOG_FILE_PROPERTY);
 
-	/** Property allowing definition of NOT logging on console. */
-	private static final String LOG_CONSOLE_PROPERTY = "hemera.log.noConsole";
+    /** Property allowing definition of NOT logging on console. */
+    private static final String LOG_CONSOLE_PROPERTY = "hemera.log.noConsole";
 
-	/** Log on console. */
-	private static final boolean LOG_CONSOLE = !Boolean.getBoolean(LOG_CONSOLE_PROPERTY);
+    /** Log on console. */
+    private static final boolean LOG_CONSOLE = !Boolean.getBoolean(LOG_CONSOLE_PROPERTY);
 
-	/** Property allowing definition of log verbose level (possible values: 0 to 3, 0=no verbose, 3=highly verbose). */
-	private static final String VERBOSE_LEVEL_PROPERTY = "hemera.log.verbose";
+    /** Property allowing definition of log verbose level (possible values: 0 to 3, 0=no verbose, 3=highly verbose). */
+    private static final String VERBOSE_LEVEL_PROPERTY = "hemera.log.verbose";
 
-	/** Verbose level. */
-	private static final Level VERBOSE_LEVEL = defineVerboseLevel();
+    /** Verbose level. */
+    private static final Level VERBOSE_LEVEL = defineVerboseLevel();
 
-	/** Log formatter. */
-	static final Formatter FORMATTER = new LogFormatter();
+    /** Log formatter. */
+    static final Formatter FORMATTER = new LogFormatter();
 
-	/** The utils logger. */
-	public static final Logger utils = Logger.getLogger("Hemera.utils");
+    /** The utils logger. */
+    public static final Logger utils = Logger.getLogger("org.hemera.utils");
 
-	/****************************************************************************************/
-	/*                                                                                      */
-	/* Constructors */
-	/*                                                                                      */
-	/****************************************************************************************/
+    /****************************************************************************************/
+    /*                                                                                      */
+    /* Constructors */
+    /*                                                                                      */
+    /****************************************************************************************/
 
-	// Checks if log file is defined, in which case a log handler is created.
-	static {
-		// Gets the root logger.
-		final Logger rootLogger = LogManager.getLogManager().getLogger("");
-		rootLogger.setLevel(VERBOSE_LEVEL);
+    // Checks if log file is defined, in which case a log handler is created.
+    static {
+        // Gets the root logger.
+        final Logger rootLogger = LogManager.getLogManager().getLogger("");
+        rootLogger.setLevel(VERBOSE_LEVEL);
 
-		// Removes default handlers.
-		for (final Handler handler : rootLogger.getHandlers()) {
-			rootLogger.removeHandler(handler);
-		}
+        // Removes default handlers.
+        for (final Handler handler : rootLogger.getHandlers()) {
+            rootLogger.removeHandler(handler);
+        }
 
-		// Adds console handler, with Hemera Log formatter.
-		if (LOG_CONSOLE) {
-			final ConsoleHandler consoleHandler = new LogConsoleHandler();
-			consoleHandler.setFormatter(FORMATTER);
-			rootLogger.addHandler(consoleHandler);
-			consoleHandler.setLevel(VERBOSE_LEVEL);
-		}
+        // Adds console handler, with Hemera Log formatter.
+        if (LOG_CONSOLE) {
+            final ConsoleHandler consoleHandler = new LogConsoleHandler();
+            consoleHandler.setFormatter(FORMATTER);
+            rootLogger.addHandler(consoleHandler);
+            consoleHandler.setLevel(VERBOSE_LEVEL);
+        }
 
-		// Checks if log file is defined.
-		if (LOG_FILE != null && LOG_FILE.length() != 0) {
-			try {
-				// It is the case, adds a file handler.
-				final Handler logFileHandler = new FileHandler(LOG_FILE, true);
-				logFileHandler.setFormatter(new LogFormatter());
-				rootLogger.addHandler(logFileHandler);
-			} catch (final SecurityException e) {
-				System.err.println("Unable to initialize Hemera Log System.");
-				e.printStackTrace();
-			} catch (final IOException e) {
-				System.err.println("Unable to initialize Hemera Log System.");
-				e.printStackTrace();
-			}
-		}
-	}
+        // Checks if log file is defined.
+        if (LOG_FILE != null && LOG_FILE.length() != 0) {
+            try {
+                // It is the case, adds a file handler.
+                final Handler logFileHandler = new FileHandler(LOG_FILE, true);
+                logFileHandler.setFormatter(new LogFormatter());
+                rootLogger.addHandler(logFileHandler);
+            }
+            catch (final SecurityException e) {
+                System.err.println("Unable to initialize Hemera Log System.");
+                e.printStackTrace();
+            }
+            catch (final IOException e) {
+                System.err.println("Unable to initialize Hemera Log System.");
+                e.printStackTrace();
+            }
+        }
+    }
 
-	/****************************************************************************************/
-	/*                                                                                      */
-	/* Specific methods */
-	/*                                                                                      */
-	/****************************************************************************************/
+    /****************************************************************************************/
+    /*                                                                                      */
+    /* Specific methods */
+    /*                                                                                      */
+    /****************************************************************************************/
 
-	/**
-	 * @return verbose level according to {@link #VERBOSE_LEVEL_PROPERTY}.
-	 */
-	private static final Level defineVerboseLevel() {
-		final int specifiedVerbose = Integer.getInteger(VERBOSE_LEVEL_PROPERTY, 0);
-		switch (specifiedVerbose) {
-		case 1:
-			return Level.FINE;
-		case 2:
-			return Level.FINER;
-		case 3:
-			return Level.FINEST;
-		default:
-			return Level.INFO;
-		}
-	}
+    /**
+     * @return verbose level according to {@link #VERBOSE_LEVEL_PROPERTY}.
+     */
+    private static final Level defineVerboseLevel() {
+        final int specifiedVerbose = Integer.getInteger(VERBOSE_LEVEL_PROPERTY, 0);
+        switch (specifiedVerbose) {
+            case 1:
+                return Level.FINE;
+            case 2:
+                return Level.FINER;
+            case 3:
+                return Level.FINEST;
+            default:
+                return Level.INFO;
+        }
+    }
 
-	/**
-	 * Manages throwable according to verbose level.
-	 * 
-	 * @param t
-	 *            throwable to manage.
-	 */
-	public static final void manageThrowable(final Throwable t) {
-		manageThrowable(t, -1);
-	}
+    /**
+     * Manages throwable according to verbose level.
+     * 
+     * @param t
+     *            throwable to manage.
+     */
+    public static final void manageThrowable(final Throwable t) {
+        manageThrowable(t, -1);
+    }
 
-	/**
-	 * Manages throwable according to verbose level.
-	 * If an exitValue != -1 is specified, {@link System#exit(int)} is called with this value.
-	 * 
-	 * @param t
-	 *            throwable to manage.
-	 * @param exitValue
-	 *            the exit value (-1 to ignore).
-	 */
-	public static final void manageThrowable(final Throwable t, final int exitValue) {
-		// Shows stack trace only in verbose mode.
-		if (VERBOSE_LEVEL.intValue() <= Level.FINE.intValue()) {
-			t.printStackTrace();
-		}
+    /**
+     * Manages throwable according to verbose level.
+     * If an exitValue != -1 is specified, {@link System#exit(int)} is called with this value.
+     * 
+     * @param t
+     *            throwable to manage.
+     * @param exitValue
+     *            the exit value (-1 to ignore).
+     */
+    public static final void manageThrowable(final Throwable t, final int exitValue) {
+        // Shows stack trace only in verbose mode.
+        if (VERBOSE_LEVEL.intValue() <= Level.FINE.intValue()) {
+            t.printStackTrace();
+        }
 
-		if (exitValue != -1) {
-			System.exit(exitValue);
-		}
-	}
+        if (exitValue != -1) {
+            System.exit(exitValue);
+        }
+    }
 
 }
